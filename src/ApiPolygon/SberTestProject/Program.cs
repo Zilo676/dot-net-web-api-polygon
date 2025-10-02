b072ed6ff789bf9b9cbba96bb59300f9a026cc22
Zilo676 <yolp@yandex.ru>
2025-10-02 15:11:48 +0300

    feat: add second task

--- src/ApiPolygon/SberTestProject/Program.cs
+++ src/ApiPolygon/SberTestProject/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(PathHelper.GetPathCount(5,5));
+        Console.WriteLine(PathHelper.GetPathCount(10,1));
+        Console.WriteLine(PathHelper.GetPathCount(1,10));
+        Console.WriteLine(PathHelper.GetPathCount(4,6));
+        Console.WriteLine(PathHelper.GetPathCount(6,4));
+        ;
+
+    }
+}
+
+public static class PathHelper
+{
+    public static int GetPathCount(int m, int n)
+
+    {
+        if (n <= 0 || m <= 0) return 0;
+
+        int[] dp = new int[m];
+        Array.Fill(dp, 1);
+        
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[m - 1];
     }
 }
